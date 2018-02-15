8e82cac0f94510992a593e72a946599c3f3f1c76
RaimundBarbuta <reimiundbarbuta@gmail.com>
2018-02-15 15:15:53 +0000

    firstversioncommited

---
+++ routes/posts.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var uuidv4 = require('uuid/v4');
+
+// define and setup Mongoose
+// connect to the db
+mongoose.connect('mongodb://cubu:mancarica@ds235388.mlab.com:35388/blogbd')
+// create a schema for the
+var Schema = mongoose.Schema;
+
+var blogPost = new Schema ({
+  blogID: String,
+  authorID: String,
+  blogTitle: String,
+  date: Date,
+  content: String,
+  headerImage: String
+});
+
+var BlogModel = mongoose.model('Post', blogPost);
+
+/* routes handlers */
+// ######### Diplays BLOG POST INFOs ##########
+router.get('/', function(req, res, next) {
+  if(req.query.blogID){
+    BlogModel.findOne({blogID: req.query.blogID}, function(err, blogpost){
+      res.json(blogpost);
+    });
+  }else{
+    BlogModel.find({}, function(err, blogposts){
+      res.json(blogposts);
+      console.log(blogposts);
+    });
+  }
+
+});
+// ########POST NEW BLOGS######
+router.post('/', function(req, res, next) {
+  var newPost = new BlogModel ({
+    blogID: uuidv4(),
+    authorID: req.body.authorID,
+    blogTitle: req.body.blogTitle,
+    date: Date.now(),
+    content: req.body.content,
+    headerImage: req.body.headerImage
+  });
+  newPost.save(function(err){
+    if(err){
+      res.send('NOT UOKE!');
+      console.log(err);
+    }else{
+      res.send('UOKE!');
+    }
+  });
+});
+
+router.put('/', function(req, res, next) {
+  var query = {
+    blogID: req.body.blogID
+    }
+  var updatedPost = ({
+    blogID: req.body.blogID,
+    authorID: req.body.authorID,
+    blogTitle: req.body.blogTitle,
+    date: Date.now(),
+    content: req.body.content,
+    headerImage: req.body.headerImage
+  });
+  BlogModel.update(query, updatedPost, function(err, blogpost){
+    if(err){
+      res.send('NOOOOOO!')
+    }else{
+      res.json(blogpost);
+    }
+  });
+});
+
+router.delete('/', function(req, res, next) {
+  var query = {
+    blogID: req.body.blogID
+  }
+  BlogModel.deleteOne(query, function(err, blogpost){
+    if(err){
+      res.send('NOOOOOO!')
+    }else{
+      res.json(blogpost);
+    }
+  });
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var uuidv4 = require('uuid/v4');
+
+// define and setup Mongoose
+// connect to the db
+mongoose.connect('mongodb://cubu:mancarica@ds235388.mlab.com:35388/blogbd')
+// create a schema for the
+var Schema = mongoose.Schema;
+
+var users = new Schema ({
+  userID: String,
+  name: String,
+  author: Boolean,
+  authorID: String,
+  email: String,
+  username: String
+});
+
+var UserModel = mongoose.model('User', users);
+
+/* routes handlers */
+// ######### Diplays BLOG POST INFOs ##########
+router.get('/', function(req, res, next) {
+  if(req.query.userID){
+    UserModel.findOne({userID: req.query.userID}, function(err, user){
+      res.json(user);
+    });
+  }else{
+    UserModel.find({}, function(err, users){
+      res.json(users);
+      console.log(users);
+    });
+  }
+
+});
+// ########REGISTER NEW USERS######
+router.post('/', function(req, res, next) {
+  var users = new UserModel ({
+    userID: uuidv4(),
+    name: req.body.name,
+    author: req.body.author,
+    email: req.body.email,
+    username: req.body.username
+  });
+  users.save(function(err){
+    if(err){
+      res.send('User Created!');
+      console.log(err);
+    }else{
+      res.send('UOKE!');
+    }
+  });
+});
+
+router.put('/', function(req, res, next) {
+  var query = {
+    userID: req.body.userID
+    }
+    var updatedUser = ({
+      userID: req.body.userID,
+      name: req.body.name,
+      author: req.body.author,
+      email: req.body.email,
+      username: req.body.username
+    });
+  UserModel.update(query, updatedUser, function(err, user){
+    if(err){
+      res.send('NOOOOOO!')
+    }else{
+      res.send('User Updated!');
+    }
+  });
+});
+
+router.delete('/', function(req, res, next) {
+  var query = {
+    userID: req.body.userID
+  }
+  UserModel.deleteOne(query, function(err, user){
+    if(err){
+      res.send('NOOOOOO!')
+    }else{
+      res.send('User Deleted!');
+    }
+  });
+});
+
+module.exports = router;
